4412f82ff2fd2a01c6bbfa9aa40479495a9409b1
MajorX234 <majorx234@googlemail.com>
2022-03-11 13:34:17 +0100

    Add midiport registration, add dial value processing

--- include/mididial_mainwindow.h
+++ include/mididial_mainwindow.h
@@ -1,22 +1,22 @@
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
-//    This file is part of QtJack.                                            //
+//    This file is part of QtJack.                                           //
 //    Copyright (C) 2014-2015 Jacob Dawid <jacob@omg-it.works>               //
 //                  2022 <majorx234@googlemail.com>                          //
-//    QtJack is free software: you can redistribute it and/or modify          //
+//    QtJack is free software: you can redistribute it and/or modify         //
 //    it under the terms of the GNU General Public License as published by   //
 //    the Free Software Foundation, either version 3 of the License, or      //
 //    (at your option) any later version.                                    //
 //                                                                           //
-//    QtJack is distributed in the hope that it will be useful,               //
+//    QtJack is distributed in the hope that it will be useful,              //
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
 //    GNU General Public License for more details.                           //
 //                                                                           //
 //    You should have received a copy of the GNU General Public License      //
-//    along with QtJack. If not, see <http://www.gnu.org/licenses/>.          //
+//    along with QtJack. If not, see <http://www.gnu.org/licenses/>.         //
 //                                                                           //
-//    It is possible to obtain a closed-source license of QtJack.             //
+//    It is possible to obtain a closed-source license of QtJack.            //
 //    If you're interested, contact me at: jacob@omg-it.works                //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
@@ -46,6 +46,7 @@ public:
     ~MidiDialMainWindow();
 
     void setupJackClient();
+    void process(int samples) override;
 
 protected slots:
     void on_twist();
@@ -54,6 +55,7 @@ private:
     Ui::Mididial *mididial_ui;
 
     QtJack::Client _client;
+    QtJack::MidiPort _midi_out;
     QAbstractSlider *slider;
 };
 
