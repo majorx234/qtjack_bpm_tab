03b3d24a3a0369083957f94decb3c7d76fd76ee9
MajorX234 <majorx234@googlemail.com>
2022-03-14 11:12:41 +0100

    Add midi message sending with dial value

--- include/mididial_mainwindow.h
+++ include/mididial_mainwindow.h
@@ -23,6 +23,7 @@
 
 #ifndef MIDIDIAL_MAINWINDOW_H
 #define MIDIDIAL_MAINWINDOW_H
+#include <atomic>
 
 // QtJack includes
 #include <Client>
@@ -32,6 +33,13 @@
 // Qt includes
 #include <QMainWindow>
 #include "ui_mididial.h"
+
+// Midi stuff
+struct midiMessage {
+  unsigned char midiData[3];
+  int length;
+  jack_nframes_t timestamp;
+};
 
 namespace Ui {
 class Mididial;
@@ -56,7 +64,10 @@ private:
 
     QtJack::Client _client;
     QtJack::MidiPort _midi_out;
+    QtJack::MidiBuffer *_midi_out_buffer; //not used yet
     QAbstractSlider *slider;
+    std::atomic<unsigned int> _value;
+    std::atomic<int> _timestamp;
 };
 
 #endif // MIDIDIAL_MAINWINDOW_H
