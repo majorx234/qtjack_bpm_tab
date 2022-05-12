149417ef5e497f6940609d0cf19ebb3d74b49a4b
MajorX234 <majorx234@googlemail.com>
2022-05-12 23:01:56 +0200

    Restructure code

--- include/wave_widget.h
+++ include/wave_widget.h
@@ -6,20 +6,23 @@
 class WaveWidget : public QWidget
 {
   Q_OBJECT
-private:
-  double *wave_[4];
-  int wave_ofs_;
-  int wavecounter_;
+
 public:
   WaveWidget(QWidget* parent);
   ~WaveWidget();
 
+public slots:
+  void getChunk();
+
+private:
+  double *wave_[4];
+  int wave_ofs_;
+  int wavecounter_;
+  int frame_rate;
+
 protected:
   virtual void paintEvent(QPaintEvent*);
   virtual void resizeEvent(QResizeEvent *ev);
-
-public slots:
-    void getChunk();      
 };
 
 
