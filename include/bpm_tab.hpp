606bb075ce75df67afee8dccfee977b0fc69994b
MajorX234 <majorx234@googlemail.com>
2022-05-18 01:48:00 +0200

    Add License.

--- include/bpm_tab.hpp
+++ include/bpm_tab.hpp
@@ -1,5 +1,28 @@
-#ifndef BPM_TAB_H_
-#define BPM_TAB_H_
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of QtJack.                                           //
+//    Copyright (C) 2014-2015 Jacob Dawid <jacob@omg-it.works>               //
+//                  2022 <majorx234@googlemail.com>                          //
+//    QtJack is free software: you can redistribute it and/or modify         //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    QtJack is distributed in the hope that it will be useful,              //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with QtJack. If not, see <http://www.gnu.org/licenses/>.         //
+//                                                                           //
+//    It is possible to obtain a closed-source license of QtJack.            //
+//    If you're interested, contact me at: jacob@omg-it.works                //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef BPM_TAB_HPP_
+#define BPM_TAB_HPP_
 
 #include <chrono>
 #include <thread>
@@ -70,4 +93,4 @@ private:
     std::mutex audio_mutex;
 };
 
-#endif // BPM_TAB_H_
+#endif // BPM_TAB_HPP_
--- include/test/main_window_test.hpp
+++ include/test/main_window_test.hpp
@@ -1,5 +1,28 @@
-#ifndef MAIN_WINDOW_TEST_H_
-#define MAIN_WINDOW_TEST_H_
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of QtJack.                                            //
+//    Copyright (C) 2014-2015 Jacob Dawid <jacob@omg-it.works>               //
+//                                                                           //
+//    QtJack is free software: you can redistribute it and/or modify          //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    QtJack is distributed in the hope that it will be useful,               //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with QtJack. If not, see <http://www.gnu.org/licenses/>.          //
+//                                                                           //
+//    It is possible to obtain a closed-source license of QtJack.             //
+//    If you're interested, contact me at: jacob@omg-it.works                //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef MAIN_WINDOW_TEST_HPP_
+#define MAIN_WINDOW_TEST_HPP_
 
 #include <QMainWindow>
 #include "wave_widget.hpp"
@@ -18,4 +41,4 @@ protected:
   void timerEvent(QTimerEvent *event);
 };
 
-#endif // MAIN_WINDOW_TEST_H_
+#endif // MAIN_WINDOW_TEST_HPP_
