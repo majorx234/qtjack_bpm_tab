fa1866cdcaa82e12456b0f2ac24820ec4c36ed8b
MajorX234 <majorx234@googlemail.com>
2022-05-17 03:39:35 +0200

    Add license

--- include/wave_widget.h
+++ include/wave_widget.h
@@ -1,3 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of QtJack.                                           //
+//    Copyright (C) 2014-2015 Jacob Dawid <jacob@omg-it.works>               //
+//                  2022 <majorx234@googlemail.com>                          //
+//    QtJack is free software: you can redistribute it and/or modify         //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    QtJack is distributed in the hope that it will be useful,              //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with QtJack. If not, see <http://www.gnu.org/licenses/>.         //
+//                                                                           //
+//    It is possible to obtain a closed-source license of QtJack.            //
+//    If you're interested, contact me at: jacob@omg-it.works                //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef WAVE_WIDGET_H_
 #define WAVE_WIDGET_H_
 
