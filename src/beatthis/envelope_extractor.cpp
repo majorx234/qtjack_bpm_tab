e78529c43311757564eced0ac0642a34cc41dd09
MajorX234 <majorx234@googlemail.com>
2022-06-12 00:55:25 +0200

    Add class for envelope extraction

---
+++ src/beatthis/envelope_extractor.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of qtjack_bpm_tab                                    //
+//    Copyright (C) 2022 <majorx234@googlemail.com>                          //
+//                                                                           //
+//    qtjack_bpm_tab is free software: you can redistribute it and/or modify //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    qtjack_bpm_tab is distributed in the hope that it will be useful,      //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with qtjack_bpm_tab. If not, see <http://www.gnu.org/licenses/>. //
+//                                                                           //
+//    It is possible to obtain a closed-source license of QtJack.            //
+//    If you're interested, contact me at: majorx234@gmail.com               //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "envelope_extractor.hpp"
+
+
+
+EnvelopeExtractor::EnvelopeExtractor(size_t samples,
+                   unsigned int sample_rate)
+  : samples_(samples)
+  , sample_rate_(sample_rate)
+{
+}
+
+EnvelopeExtractor::~EnvelopeExtractor(){
+}
