b0f39b708be5bd64f76556e93f040ceb3ca13c4d
MajorX234 <majorx234@googlemail.com>
2022-06-12 17:38:19 +0200

    Add license

--- src/beatthis/filterbank.cpp
+++ src/beatthis/filterbank.cpp
@@ -1,3 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of qtjack_bpm_tab                                    //
+//    Copyright (C) 2022 <majorx234@googlemail.com>                          //
+//                                                                           //
+//    qtjack_bpm_tab is free software: you can redistribute it and/or modify //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    qtjack_bpm_tab is distributed in the hope that it will be useful,      //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with qtjack_bpm_tab. If not, see <http://www.gnu.org/licenses/>. //
+//                                                                           //
+//    It is possible to obtain a closed-source license of QtJack.            //
+//    If you're interested, contact me at: majorx234@gmail.com               //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
 #include <math.h>
 #include <cassert>
 #include <cstring>
@@ -55,7 +78,7 @@ float** Filterbank::filter_signal(float* in_buffer,
     output[i] = (float*)malloc((samples_)*sizeof(float*));
     memset(output[i],0,(samples_)*sizeof(float*));
   }
-  
+
   unsigned int bl[nbands];
   unsigned int br[nbands];
   
